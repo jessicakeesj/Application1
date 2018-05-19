b145f04c1ec7258ab72982f69607c3daf4a0ef1a
syongkheng <syongkheng@users.noreply.github.com>
2018-05-19 17:14:12 +0800

    commit - main

--- app/src/main/java/applications/application1/MainActivity.java
+++ app/src/main/java/applications/application1/MainActivity.java
@@ -2,12 +2,63 @@ package applications.application1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] IMAGES = {R.drawable.splash};
+
+    String[] NAME = {"Consumer Electronics Exhibition"};
+
+    String[] DESC = {"Hello World"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView=(ListView)findViewById(R.id.listView);
+
+        CustomAdapter customAdapter=new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return IMAGES.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.customlayout,null);
+
+            ImageView imageView=(ImageView)view.findViewById(R.id.imageView);
+            TextView textView_name = (TextView)view.findViewById(R.id.textView_name);
+            TextView textView_desc = (TextView)view.findViewById(R.id.textView_desc);
+
+            imageView.setImageResource(IMAGES[i]);
+            textView_name.setText(NAME[i]);
+            textView_desc.setText(DESC[i]);
+            return view;
+        }
     }
 }
