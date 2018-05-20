2e4c852a9c4e87cb22693c15505dd80edc4f107f
Jessica Kee <jksj98@gmail.com>
2018-05-20 22:18:01 +0800

    commit - search

--- app/src/main/java/applications/application1/EventsFragment.java
+++ app/src/main/java/applications/application1/EventsFragment.java
@@ -3,12 +3,16 @@ package applications.application1;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +47,8 @@ public class EventsFragment extends Fragment {
 
         return view;
     }
+
+
 
     class CustomAdapter extends BaseAdapter {
 
--- app/src/main/java/applications/application1/MainActivity.java
+++ app/src/main/java/applications/application1/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,8 +44,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem((R.id.menuSearch));
+        SearchView searchView = (SearchView)  item.getActionView();
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
 }
